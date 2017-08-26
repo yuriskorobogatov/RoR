2a83b056e010af5280b3dd539400fbec357d13ad
yuriskorobogatov <yuriskorobogatov1991@gmail.com>
2017-08-26 23:43:43 +0500

    Lesson 3

--- Задание к уроку Основы Ruby. Часть 3/train.rb
+++ Задание к уроку Основы Ruby. Часть 3/train.rb
@@ -1,5 +1,6 @@
 class Station
-  attr_accessor :name, :trains, :pas, :wei
+  attr_accessor :name, :trains
+
   def initialize(name)
     @name = name
     @trains = []
@@ -9,15 +10,10 @@ class Station
     @trains << train
   end
 
-  def count_pas
-    @pas = []
-    trains.each { |k| pas.push(k) if k.type == 'passenger' }
+  def trains_by_type
+    trains.select{|k| k.type}
   end
 
-  def count_wei
-    @wei = []
-    trains.each { |k| wei.push(k) if k.type == 'freight' }
-  end
 
   def depart_train(train)
     trains.delete(train)
@@ -25,86 +21,91 @@ class Station
 end
 
 class Route
-  attr_accessor :first_st, :lats_st, :else_st
-  def initialize(first_st,lats_st)
-    @first_st = first_st
-    @lats_st = lats_st
-    @else_st = []
+  attr_accessor :first_station, :lats_station
+  attr_reader :stations
+
+  def initialize(first_station,lats_station)
+    @stations = [first_station, lats_station]
+      end
+
+  def add_station(new_station)
+    @stations << new_station
   end
-  def add_st(new_st)
-    @else_st << new_st
-  end
-  def delete_st(new_st)
-    else_st.delete(new_st)
-  end
-  def show_st
-    puts "#{@first_st}" + else_st.each {|k| puts k} + "#{@lats_st}"
+
+  def delete_station(new_station)
+    @stations.delete(new_station)
   end
 end
 
 
 class Train
-  attr_accessor :v_count, :speed
-  def initialize(number, type, v_count)
+  attr_accessor :number_wagons, :speed
+
+  def initialize(number, type, number_wagons)
     @number = number
     @speed = 0
     @type = type
-    @v_count = v_count.to_i
+    @number_wagons = number_wagons.to_i
   end
 
-  def accelerate_by
+  def accelerate
     @speed += 1
   end
+
   def stop
     @speed = 0
   end
 
+  def current_station
+    station(@station_index)
+  end
+
   def assign_route(route)
     @route = route
-    @route.first_st.add_st(self)
+    @route.current_station.add_train(self)
     @station_index = 0
   end
 
-  def current_station
-    @route.else_st[@station_index] if @route
+  def add_train(n)
+    @number_wagons = @number_wagons + n if speed==0
   end
 
-  def add_v(n)
-    @v_count = @v_count.to_i + n.to_i if speed==0
-  end
-  def remove_v(n)
-    @v_count = @v_count.to_i - n.to_i if speed==0
+  def remove_train(n)
+    @number_wagons = @number_wagons - n if speed==0
   end
 
-  def next_st?
-    @station_index < @route.else_st.length
+  def next_station?
+    @station_index < @route.station.length
   end
 
-  def prev_st?
+  def prev_station?
     @station_index > 0
   end
 
   def move_back
-    return unless prev_st?
-    current_station.delete_st(self)
+    return unless prev_station?
+    current_station.remove_train(self)
     @station_index -= 1
-    current_station.add_st(self)
+    current_station.add_train(self)
   end
 
   def move_next
-    return unless next_st?
-    current_station.delete_st(self)
+    return unless next_station?
+    current_station.remove_train(self)
     @station_index += 1
-    current_station.add_st(self)
+    current_station.add_train(self)
   end
 
-  def next_st
-    @route.else_st[@station_index + 1]
+  
+  def next_station
+    @route.station[@station_index + 1]
   end
 
-  def prev_st
-    @route.else_st[@station_index - 1] if @station_index > 0
+  def prev_station
+    @route.station[@station_index - 1] if @station_index > 0
   end
 
-
+  def station(index)
+    @route.station[index]
+  end
 end
